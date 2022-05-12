83de20de2a4786bbd1ba89863ac5ed62c97262e1
delcidj22 <jdelcid23@gmail.com>
2022-05-11 20:46:33 -0700

    Initial Commit/ Adding PowerPlant files

---
+++ src/js/plant.js
@@ -0,0 +1,52 @@
+// This funciton stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFuntion = state => state) => {
+    const newState = stateChangeFuntion(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+// This is a funciton factory. We can easily create more specific functions that alter a plant's soil, water, and light to carying degrees.
+
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+// We create four functions using our funciton factory. We could easily create many more.
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
+$(document).ready(function() {
+
+  // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+
+  $('feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+
+  // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+
+
+  $('#show-state').click(function() {
+    // We just need to call stateControl() without arguments to see our current state.
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
